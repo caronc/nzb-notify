d8399101900e049dd1e128752fdfe97c02773260
Chris Caron <lead2gold@gmail.com>
2015-01-15 19:43:05 -0500

    version increment

--- Notify.py
+++ Notify.py
@@ -30,9 +30,9 @@
 #
 # Info about this Notify NZB Script:
 # Author: Chris Caron (lead2gold@gmail.com).
-# Date: Tue, Dec 29th, 2014.
+# Date: Tue, Jan 15th, 2015.
 # License: GPLv2 (http://www.gnu.org/licenses/gpl.html).
-# Script Version: 0.1.7
+# Script Version: 0.1.8
 #
 
 ###########################################################################
